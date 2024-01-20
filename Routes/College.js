0a081cc50b52a372a5c9ebb3742618ec172ab0a9
Harsh Godkar <harshgodkar14@gmail.com>
2024-01-20 11:32:58 +0530

    commit

--- Routes/College.js
+++ Routes/College.js
@@ -401,4 +401,111 @@ router.post('/schedulematches', async(req,res) => {
   }
 });
 
+router.post('/getScheduledEvents', async (req, res) => {
+  try {
+    const matches = await Match.find({ event: { $exists: true } }, 'event');
+
+    // Extract unique event IDs from the matches
+    const eventIds = matches.map(match => match.event);
+
+    // Retrieve all events using the event IDs
+    const events = await event.find({ _id: { $in: eventIds } });
+
+    res.json({ events });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.post('/getMatches/:eventId', async (req, res) => {
+  try {
+    const eventId = req.params.eventId;
+    // console.log(eventId);
+
+    const matches = await Match.find({ event: eventId });
+
+    // Use Promise.all to wait for all asynchronous operations to complete
+    const matchesWithDetails = await Promise.all(
+      matches.map(async (match) => {
+        const eventName = await event.findById(match.event);
+        const clg1 = await Clg.findById(match.clg1);
+        const clg2 = await Clg.findById(match.clg2);
+        let winn = '';
+
+        if(match.winner){
+          winn = await Clg.findById(match.winner);
+        }
+
+        return {
+          eventName : eventName.event_name,
+          clg1 : clg1,
+          clg2 : clg2,
+          id: match._id,
+          match_date: match.match_date,
+          time: match.time,
+          winner : winn.clg_name
+        };
+      })
+    );
+
+    // console.log(matchesWithDetails);
+    res.json({ matches: matchesWithDetails });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.post('/matchWinner/:eventId/:index', async (req, res) => {
+  try {
+    const index = req.params.index;
+    const {clg} = req.body; // Assuming clgId is a unique identifier like the clg._id
+    const eventId = req.params.eventId;
+
+    const matches = await Match.find({ event: eventId });
+    
+    // Make sure the index is within bounds
+    if (index >= 0 && index < matches.length) {
+      // Determine the winner based on the clgId
+      let winner;
+      if (clg.toString() === matches[index].clg1.toString()) {
+        winner = matches[index].clg1;
+      } else if (clg.toString() === matches[index].clg2.toString()) {
+        winner = matches[index].clg2;
+      }
+
+      // Update the winner property for the specific match
+      // matches[index].winner = winner;
+      const match = new Match({
+        _id : matches[index]._id,
+        clg1 : matches[index].clg1,
+        clg2 : matches[index].clg2,
+        event : matches[index].event,
+        match_date : matches[index].match_date,
+        time: matches[index].time,
+        winner : winner
+      });
+
+      // console.log("Match :: ")
+      // console.log(match)
+      // Save the updated match back to the database
+      await Match.findOneAndUpdate({ _id: matches[index]._id }, match)
+        .then(() => {
+          res.json({ success: true });
+        })
+        .catch((e) => {
+          console.log(e);
+          res.json({ success: false });
+        });
+    } else {
+      res.json({ success: false, error: "Index out of bounds" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
 module.exports = router;
