620c13e3b7cf412db28e93cb3d05c290ddf4f0b9
{Akshar1199} <{aksharparekh1199@gmail.com}>
2024-02-16 13:38:30 +0530

    commit

--- Routes/College.js
+++ Routes/College.js
@@ -564,7 +564,9 @@ router.post("/matchWinner/:eventId/:index", async (req, res) => {
       if (clgstatexist1) {
         clgstatexist1.wins = clgstatexist1.wins + 1;
         clgstatexist1.total_matches = clgstatexist1.total_matches + 1;
-      } else {
+        
+      } 
+      else {
         const response1 = await axios.post(
           "http://localhost:5000/clg/getOrganizeCollege"
         );
